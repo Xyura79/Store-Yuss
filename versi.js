d1a35cca62a967b2f9bd3ecd299bc6c73062051f
YussXy <tectmount@gmail.com>
2026-05-23 21:24:55 +0700

    Add files via upload

---
+++ versi.js
@@ -0,0 +1,40 @@
+// ================================================
+// FILE: version.js
+// Menyimpan informasi versi aplikasi
+// ================================================
+
+const APP_VERSION = "3.3.5";
+
+
+const APP_RELEASE_DATE = "16 Mei 2026";
+
+
+const APP_UPDATE_INFO = "Jangan Lupa Follow Saluran Resmi Kami Agar Tidak Ketinggalan Info update, promo, dan informasi penting lainnya.";
+
+
+
+const NOTIFICATION_DATA = {
+    hasNotification: true,
+    count: 1,
+    notifications: [
+        {
+            id: 1,
+            title: "Update Versi",
+            message: "Aplikasi YussXy telah diperbarui ke versi 3.0.0",
+            image: "",
+            date: "16 Mei 2026"
+        },
+                {
+            id: 3,
+            title: "Produk Baru Di Tambah",
+            message: "Aplikasi Pembuat Logo JB",
+            image: "image/produk/produk18.jpg",
+            date: "22 Mei 2026"
+        },
+        
+        
+        
+        
+        
+    ]
+};
