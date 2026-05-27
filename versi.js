e3fc115bfb67f32a048b3be0521ab7258e9089cc
YussXy <tectmount@gmail.com>
2026-05-27 20:27:26 +0700

    Update application version to 3.7.0

--- versi.js
+++ versi.js
@@ -3,7 +3,7 @@
 // Menyimpan informasi versi aplikasi
 // ================================================
 
-const APP_VERSION = "3.5.0";
+const APP_VERSION = "3.7.0";
 
 
 const APP_RELEASE_DATE = "16 Mei 2026";
