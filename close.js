587a5384d08d022c14fd88ff393edf3fb4773fc5
YussXy <tectmount@gmail.com>
2026-05-29 23:10:45 +0700

    Set store to closed and update close reason

--- close.js
+++ close.js
@@ -3,12 +3,12 @@
 // ================================================
 
 // ========== KONFIGURASI - UBAH DI SINI ==========
-const STORE_CLOSED = false;  // true = tutup, false = buka
+const STORE_CLOSED = true;  // true = tutup, false = buka
 
-const ESTIMATED_OPEN_HOUR = "19.00";
+const ESTIMATED_OPEN_HOUR = "08.00";
 
 
-const CLOSE_REASON = "istirahat";
+const CLOSE_REASON = "Nguantukk Mau Bobok";
 
 // PESAN LENGKAP (otomatis tergabung)
 const CLOSE_MESSAGE = `Haloo, Saya close dulu ya. Ini dilakukan untuk menghindari orderan yang lama diproses. Kamu masih bisa menggunakan tools di aplikasi ini kok`;
