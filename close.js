f16089ac77d36f0d9d5be6e2ac49b322b060a237
YussXy <tectmount@gmail.com>
2026-05-30 23:06:50 +0700

    Update close.js

--- close.js
+++ close.js
@@ -3,12 +3,12 @@
 // ================================================
 
 // ========== KONFIGURASI - UBAH DI SINI ==========
-const STORE_CLOSED = false;  // true = tutup, false = buka
+const STORE_CLOSED = true;  // true = tutup, false = buka
 
-const ESTIMATED_OPEN_HOUR = "16.20";
+const ESTIMATED_OPEN_HOUR = "08.00/13.00";
 
 
-const CLOSE_REASON = "Mau ada urusan dulu sebentar";
+const CLOSE_REASON = "Ngantuk Mau tidur";
 
 // PESAN LENGKAP (otomatis tergabung)
 const CLOSE_MESSAGE = `Haloo, Saya close dulu ya. Ini dilakukan untuk menghindari orderan yang lama diproses. Kamu masih bisa menggunakan tools di aplikasi ini kok`;
