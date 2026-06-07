590b8516dc95600325726969754b7699cdaa8d0b
YussXy <tectmount@gmail.com>
2026-06-07 15:01:12 +0700

    Update app version and release date in versi.js

--- versi.js
+++ versi.js
@@ -1,17 +1,15 @@
+
+
 // ================================================
 // FILE: version.js
 // Menyimpan informasi versi aplikasi
 // ================================================
 
-const APP_VERSION = "3.9.6";
+const APP_VERSION = "3.7.0";
 
-
-const APP_RELEASE_DATE = "07 Juni 2026";
-
+const APP_RELEASE_DATE = "16 Mei 2026";
 
 const APP_UPDATE_INFO = "Jangan Lupa Follow Saluran Resmi Kami Agar Tidak Ketinggalan Info update, promo, dan informasi penting lainnya.";
-
-
 
 const NOTIFICATION_DATA = {
     hasNotification: true,
@@ -22,27 +20,21 @@ const NOTIFICATION_DATA = {
             title: "Update Versi",
             message: "Aplikasi YussXy telah diperbarui ke versi 3.0.0",
             image: "",
-            date: "16 Mei 2026"
+            date: "20 Mei 2026"
         },
-                {
+        {
             id: 3,
             title: "Produk Baru Di Tambah",
             message: "Aplikasi Pembuat Logo JB",
             image: "image/produk/produk18.jpg",
             date: "22 Mei 2026"
-        },
-
-        {
-            id: 4,
-            title: "Update Di Tambahkan Di Bagian Qris",
-            message: "Sekarang Qris Sudah tidak memakai link, sekarang langsung ke path foto nya, membuat qris tidak error, dan sekarang jika kalian klik Qris nya itu akan membuat qris nya membesar.",
-            image: "image/qris/qris.jpg",
-            date: "7 Juni 2026"
-        },
-        
-        
-        
-        
-        
+        }
     ]
 };
+
+
+const BANNED_DEVICE_IDS = [
+    // "A1B2C3D4E5F6G7H8",
+    "6D4C4AE7C3AF2F99",
+    
+];
