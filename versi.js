f638730933abfbe593f67605402fd93f92b1f067
YussXy <tectmount@gmail.com>
2026-06-08 11:33:43 +0700

    Update versi.js

--- versi.js
+++ versi.js
@@ -7,7 +7,7 @@
 
 const APP_VERSION = "3.9.9";
 
-const APP_RELEASE_DATE = "7 juni 2026";
+const APP_RELEASE_DATE = "8 juni 2026";
 
 const APP_UPDATE_INFO = "Jangan Lupa Follow Saluran Resmi Kami Agar Tidak Ketinggalan Info update, promo, dan informasi penting lainnya.";
 
